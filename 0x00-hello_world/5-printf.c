ead421fa921f3036009d5dd87b4302b3538c0a61
AGE103 <adegboyega.ema@gmail.com>
2022-09-16 22:43:17 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in thr put function
+ *
+ * description: using the main function
+ * this program prints an input
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of are,\n");
+		return (0);
+}
