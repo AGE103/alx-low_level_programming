0797dab4508410c414a2c8d696de6900d67a7fb8
AGE103 <adegboyega.ema@gmail.com>
2022-09-17 15:41:55 -0700

    final size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - print the string in the function
+ * main - entry point
  *
- * Description: using the mainfunction
- * this program prints the input
- * Return: 0
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-printf("size of a char: %d byte(s)\n", sizeof(char));
-printf("size of an int: %d byte(s)\n", sizeof(int));
-printf("size of a long int: %d byte(s)\n", sizeof(long));
-printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("size of a float: %d byte(s)\n", sizeof(float));
-return(0);
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+return: (0)
 }
