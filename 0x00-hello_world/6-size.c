4b585f9e9ea94629bd81b3f8e9ab1d4714ee3393
AGE103 <adegboyega.ema@gmail.com>
2022-09-16 23:09:28 -0700

    sizw

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the function
+ *
+ * Description: using the mainfunction
+ * this program prints the input
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	
+	printf("size of a char: %1d bytes(s)\n",sizeof(c));
+	printf("size of an int: %1d bytes(s)\n",sizeof(i));
+	printf("size of a long: %1d bytes(s)\n",sizeof(li));
+	printf("size of a long long:%1d bytes(s)\n",sizeof(lli);
+	printf)"size of a float: %1d byte(s)\n"sizeof(f));
+	return (0);
+}
