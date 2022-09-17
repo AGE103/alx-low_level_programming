4009ad9774df55ff6fb109cb7a22bde2c989b2f0
AGE103 <adegboyega.ema@gmail.com>
2022-09-16 23:13:46 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("size of a char: %1d bytes(s)\n",sizeof(c));
 	printf("size of an int: %1d bytes(s)\n",sizeof(i));
 	printf("size of a long: %1d bytes(s)\n",sizeof(li));
-	printf("size of a long long:%1d bytes(s)\n",sizeof(lli);
+	printf("size of a long long:%1d bytes(s)\n",sizeof(lli));
 	printf)"size of a float: %1d byte(s)\n"sizeof(f));
 	return (0);
 }
