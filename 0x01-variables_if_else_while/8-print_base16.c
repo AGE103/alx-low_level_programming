ce95ab29ec89696ea2c12f3e2ef74f4f08c5ef14
AGE103 <adegboyega.ema@gmail.com>
2022-09-21 21:53:14 -0700

    C program t
    hat prints all the numbers of base 16 in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,12 +10,12 @@ int main(void)
 		int num;
 		char letter;
 
-					for (num = '0'; num <= '9'; num++)
+				for (num = '0'; num <= '9'; num++)
 			{
 				putchar(num);
 					}
 
-					for (letter = 'a'; letter <= 'f'; letter++)
+				for (letter = 'a'; letter <= 'f'; letter++)
 			{
 				putchar(letter);
 					}
