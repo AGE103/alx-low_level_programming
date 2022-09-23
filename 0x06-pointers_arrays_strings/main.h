30808fa091730999764b845df0cbb08c7de4b154
AGE103 <adegboyega.ema@gmail.com>
2022-09-22 17:55:04 -0700

    main h file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,20 +2,18 @@
 #define MAIN_H
 
 /**
-*
 *void prototypes()
 *
-*int prototypes()
+**int prototypes()
 *
 * char prototype()
 *
 */
-
 int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
 
-ar *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
@@ -37,7 +35,6 @@ void *print_buffer(char *b, int size);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
-char *add_strings(char *n1< char  *n2< char *r, int r_index);
 
 
 #endif
