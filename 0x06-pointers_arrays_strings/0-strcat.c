b2aae64931fc4f5099ca38c5c5dac32bd6523363
AGE103 <adegboyega.ema@gmail.com>
2022-09-22 17:41:10 -0700

    task o

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#incclude "main.h"
+#include "main.h"
 
 /**
 *
@@ -11,7 +11,7 @@
 *
 */
 
-char *_strcat(char *dest, char *src)                
+char *_strcat(char *dest, char *src)
 
 {
 	int index = 0, dest_len = 0;
@@ -20,8 +20,8 @@ char *_strcat(char *dest, char *src)
 			dest_len++;
 
 		for (index = 0; src[index]; index++)
-		       	dest[dest_len++] = src[index++];
-		
+			dest[dest_len++] = src[index++];
+
 	return (dest);
 
 }
