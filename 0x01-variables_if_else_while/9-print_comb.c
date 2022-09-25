66f81ead461cc10fef8f512ef0a90b0762259419
AGE103 <adegboyega.ema@gmail.com>
2022-09-25 09:24:29 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int num = 48;
 
-	while (num <= 57)
-	{
+		while (num <= 57)
+		{
 			putchar(num);
-	
+
 		if (num != 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 			num++;
-	}
+		}
 	putchar('\n');
 
 	return (0);
