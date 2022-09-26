3eaff8b707bd79ac1f66c13eb55d7cc5cf95dc3d
AGE103 <adegboyega.ema@gmail.com>
2022-09-25 19:05:48 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,34 +16,35 @@ void times_table(void)
 
 	int z; /* computed value */
 
-		while (x < 10)
-		{
-			y = 0;
-			while (y < 10)
-			{
-				z = x * y;
+	while (x < 10)
+	{
+		y = 0;
 
-				if (z > 9)
-				{
-					_putchar(z / 10 + '0');
-					_putchar(z % 10 + '0');
-				}
-				else if (y != 0)
-				{
-					_putchar(' ');
-					_putchar(z + '0');
-				}
-				else
-					_putchar(z + '0');
+		while (y < 10)
+		{
+			z = x * y;
+			if (z > 9)
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
 			}
 			if (y != 9)
-				{
+			{
 				_putchar(',');
 				_putchar(' ');
-				}
-				y++;
-				{
-				_putchar('\n');
-				x++;
-				}
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
 }
