20647b05fbe70dc7068f6fd85751623d81b03712
AGE103 <adegboyega.ema@gmail.com>
2022-09-26 18:58:49 -0700

    Task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,29 @@
 #include "main.h"
 
 /**
- *_strchr- a function that locates a char in a string
- *@s: a pointer given by main
- *@c: the char to look for
- *Description: returns the first occurance of the char c in string
- *Return: returns a pointer to the first occurance
+ *_strstr - ffind the first occurrence
+ *
+ *@haystack: string
+ *
+ *@needle: substring
+ *
+ *Return: a pointer to needle or NULL if no byte is found
  */
 
 char *_strstr(char *haystack, char *needle)
 
 {
-
 	while (*haystack != 0)
 	{
 		char *a = haystack;
 
 		char *b = needle;
 
-	while (*haystack == *b && *b != 0 && *haystack != 0)
-		haystack++, b++;
-	if (*b == 0)
-		return (a);
-	haystack = ++a;
+		while (*haystack == *b && *b != 0 && *haystack != 0)
+			haystack++, b++;
+		if (*b == 0)
+			return (a);
+		haystack = ++a;
 	}
 	return (0);
 }
